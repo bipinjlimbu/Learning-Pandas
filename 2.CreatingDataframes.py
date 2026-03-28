0bcab4f022be4fdca4164a0416692970ede98bf0
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-28 19:13:15 +0545

    Creating Dataframe from a list of Dictionaries in Pandas

--- 2.CreatingDataframes.py
+++ 2.CreatingDataframes.py
@@ -10,3 +10,15 @@ data = {
 print("DataFrame from a dictionary:\n")
 df_from_dict = pd.DataFrame(data)
 print(df_from_dict)
+
+# Creating a DataFrame from a list of dictionaries
+data_list = [
+    {'Name': 'Alice', 'Age': 25, 'City': 'New York'},
+    {'Name': 'Bob', 'Age': 30, 'City': 'Los Angeles'},
+    {'Name': 'Charlie', 'Age': 35, 'City': 'Chicago'},
+    {'Name': 'David', 'Age': 40, 'City': 'Houston'},
+    {'Name': 'Eve', 'Age': 45, 'City': 'Phoenix'}
+]
+print("\nDataFrame from a list of dictionaries:\n")
+df_from_list = pd.DataFrame(data_list)
+print(df_from_list)
