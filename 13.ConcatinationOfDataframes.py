85e6c18ca9ea158c6b51a75eb3d719bc3f5ff5e9
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-29 16:45:54 +0545

    Conacatination of Two Dataframes Vertically in Pandas

---
+++ 13.ConcatinationOfDataframes.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+data1 = {
+    'A':['A0', 'A1', 'A2', 'A3'],
+    'B':['B0', 'B1', 'B2', 'B3'],
+    'C':['C0', 'C1', 'C2', 'C3'],
+    'D':['D0', 'D1', 'D2', 'D3']
+}
+
+data2 = {
+    'A':['A4', 'A5', 'A6', 'A7'],
+    'B':['B4', 'B5', 'B6', 'B7'],
+    'C':['C4', 'C5', 'C6', 'C7'],
+    'D':['D4', 'D5', 'D6', 'D7']
+}
+
+df1 = pd.DataFrame(data1)
+df2 = pd.DataFrame(data2)
+
+# Concatenating DataFrames vertically (default)
+print("Concatenated DataFrame (vertical):\n")
+concatenated_vertical = pd.concat([df1, df2]).reset_index(drop=True)
+print(concatenated_vertical)
