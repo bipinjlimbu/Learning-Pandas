571f27f7c50276bfae4d57706469ddd27b521f3f
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-30 18:56:00 +0545

    Feature Extraction Project for Extracting Episodes Number from Title in Pandas

---
+++ Projects/Project1.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from CSV import df
+
+# Feature Extraction: Extracting the number of episodes from the 'Title' column
+
+def extract_episode(txt):
+    check = False
+    data = ''
+    for i in txt:
+        if i == ')':
+            break
+        
+        if i == '(':
+            check = True
+            continue
+            
+        if check:
+            data += i
+              
+    return data
+
+df['Episodes'] = df['Title'].apply(lambda x: extract_episode(x))
+df['Episodes'] = df['Episodes'].str.replace(' eps', '').astype(int)
+print(df)
