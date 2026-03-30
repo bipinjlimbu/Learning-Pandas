6fdd7a1dc77f38261478a1a449e497e449b77501
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-30 20:20:47 +0545

    Anime CSV Read Fixed in Pandas

--- Projects/FeatureExtractionProject/anime_csv.py
+++ Projects/FeatureExtractionProject/anime_csv.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\Dell\Desktop\Pandas\Projects\Feature Extraction Project\anime.csv')
+df = pd.read_csv(r'C:\Users\Dell\Desktop\Pandas\Projects\FeatureExtractionProject\anime.csv')
