cf532f010d7e2d39c4f1d1346d09949932ec47f8
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-30 19:10:15 +0545

    Feature Extraction Project for Extracting Timestamp from Title in Pandas

---
+++ Projects/Project2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from Project1 import df
+
+def extract_timestamp(txt):
+    data = ''
+    for i in range(len(txt)):
+        if txt[i] == ')':
+            for j in range(i+1, i+20):
+                data += txt[j]
+                
+    return data
+
+df['Timestamp'] = df['Title'].apply(lambda x: extract_timestamp(x))
+
+if __name__ == "__main__":
+    print("\nFirst 5 rows of the DataFrame with Timestamp:\n")
+    print(df.head())
