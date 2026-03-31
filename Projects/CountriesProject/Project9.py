bd97c1a9b14efb813137a92405430b454b9dc39e
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-31 20:01:42 +0545

    Finding the number of countries with Republic in their name in Pandas

--- Projects/CountriesProject/Project9.py
+++ Projects/CountriesProject/Project9.py
@@ -3,5 +3,5 @@ from countries_csv import df
 
 # Finding the number of countries with 'Republic' in their name
 
-count = df['country'].str.contains('Republic').sum()
+count = df['country_long'].str.contains('Republic').sum()
 print(f"Number of countries with 'Republic' in their name: {count}")
