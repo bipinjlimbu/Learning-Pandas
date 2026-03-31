0ab990b1f96e9263d08dbe4d0bc39ae9c0a1651b
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-31 08:29:05 +0545

    Finding Capital City of the Country with the highest population in Pandas

--- Projects/CountriesProject/Project1.py
+++ Projects/CountriesProject/Project1.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from countries_csv import df
 
-# Finding the country with the highest population
+# Finding the capital city of the country with the highest population
 
 max = df['population'] == df['population'].max()
-print("\nCountry with the highest population:\n")
-print(df['country'][max])
+print("\nCapital City of the Country with the highest population:\n")
+print(df['capital_city'][max])
