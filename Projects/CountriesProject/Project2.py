85576bc32a68bc847284c7fea564549cb6553358
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-31 08:30:32 +0545

    Finding the Country with the lowest population in Pandas

--- Projects/CountriesProject/Project2.py
+++ Projects/CountriesProject/Project2.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from countries_csv import df
 
+# Finding the country with the lowest population
+
 min = df['population'] == df['population'].min()
 print("\nCountry with the lowest population:\n")
 print(df['country'][min])
